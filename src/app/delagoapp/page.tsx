bb80842f75aaf6b3c878f627aad17d526597910a
Abraham Alfonso Alemán <abrahamalfonsoaleman@gmail.com>
2025-07-29 21:42:39 +0000

    pon primero la imagen y despues el contenido escrito y los botones, recu

--- src/app/delagoapp/page.tsx
+++ src/app/delagoapp/page.tsx
@@ -24,7 +24,15 @@ export default function DelagoAppPage() {
             <main className="flex-1">
                 <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
                     <div className="container px-4 md:px-6">
-                        <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+                        <div className="grid gap-6 lg:grid-cols-[400px_1fr] lg:gap-12 xl:grid-cols-[400px_1fr]">
+                            <Image
+                                src="https://play-lh.googleusercontent.com/zkLw3flPo9YpNoXjcMBmwqNWitpPkiSK3SKiHDVJovZZLoybM3_Xwx8Qi6c5APPXogc=w240-h480-rw"
+                                alt="DelagoApp Logo"
+                                width={300}
+                                height={300}
+                                className="mx-auto aspect-square overflow-hidden rounded-xl object-cover"
+                                data-ai-hint="app logo"
+                            />
                             <div className="flex flex-col justify-center space-y-4">
                                 <div className="space-y-2">
                                     <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none font-headline">
@@ -45,14 +53,6 @@ export default function DelagoAppPage() {
                                     </Button>
                                 </div>
                             </div>
-                            <Image
-                                src="https://play-lh.googleusercontent.com/zkLw3flPo9YpNoXjcMBmwqNWitpPkiSK3SKiHDVJovZZLoybM3_Xwx8Qi6c5APPXogc=w240-h480-rw"
-                                alt="DelagoApp Logo"
-                                width={400}
-                                height={400}
-                                className="mx-auto aspect-square overflow-hidden rounded-xl object-cover sm:w-full lg:order-last"
-                                data-ai-hint="app logo"
-                            />
                         </div>
                     </div>
                 </section>
