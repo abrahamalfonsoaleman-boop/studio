4c59198c9d4e72c769b72efb60506a418df2afb6
Abraham Alfonso Alemán <abrahamalfonsoaleman@gmail.com>
2025-07-29 19:13:51 +0000

    hablo de esto

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -27,7 +27,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center">
+      <div className="flex h-14 items-center px-4 md:px-6">
         <Link href="/" className="mr-6 flex items-center space-x-2">
           <Image src="/images/Logo-vector.png" alt="Club de Lago Logo" width={40} height={40} className="transition-transform duration-300 group-hover:rotate-12" />
           <span className="font-bold font-headline text-base text-accent transition-colors duration-300 hover:text-primary">Club de Lago</span>
